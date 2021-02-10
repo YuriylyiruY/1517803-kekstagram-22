61c928f38e76a9ff49a12a6b76a570d636a326b5
YuriylyiruY <ykomendantskiy@list.ru>
2021-02-10 19:39:17 +0400

    доработка кода1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
+'use strict';
 const getRandomBetween = function (fromNum, toNum) {
 
   if (isNaN(toNum) || isNaN(fromNum) || Array.isArray(toNum) || Array.isArray(fromNum) || fromNum < 0 || toNum < 0) {
     throw new Error('Передано неверное значение.');
   }
 
+  let max = fromNum;
+  let min = toNum;
   if (fromNum > toNum) {
-    let max = fromNum;
-    let min = toNum;
-    let rand = (Math.random() * (max + 1 - min) + min);
-    return Math.floor(rand);
+    [min, max] = [max, min];
   }
-  let rand = (Math.random() * (toNum + 1 - fromNum) + fromNum);
-  return Math.floor(rand);
-
+  return Math.floor((Math.random() * (max + 1 - min) + min));
 }
 
 //task 2
@@ -22,54 +20,53 @@ const checkStringLength = function (STR, MAXLENGTH) {
   }
 
 }
-
+const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 const getMessageText = () => {
-  const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'В целом всё неплохо. Но не всё.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-  ];
+
   const randArray = getRandomBetween(0, MESSAGE.length - 1);
   return (MESSAGE[randArray]);
 };
 
-const ourComments = (i) => {
-  'use strict';
-  const getComment = {
-    id: getRandomBetween(15, 200),
-    avatar: `img/avatar-${getRandomBetween(1, 6)}.jpg`,
+const getOurComments = (i) => {
+
+  const START_NUM_ID = 15;
+  const END_NUM_ID = 200;
+  const START_NUM_AVATAR = 1;
+  const END_NUM_AVATAR = 6;
+  const COMMENT = {
+    id: getRandomBetween(START_NUM_ID, END_NUM_ID),
+    avatar: `img/avatar-${getRandomBetween(START_NUM_AVATAR, END_NUM_AVATAR)}.jpg`,
     message: getMessageText(),
     name: `user${i}`,
   }
+  return COMMENT;
 
-  return (Object.entries(getComment)
-    .map(([key, value]) => key + ' — ' + value)
-    .join(', '));
 }
-
-
 const STR = 'good';
 const MAXLENGTH = 9;
-checkStringLength(STR,MAXLENGTH);
 const MIN = 15;
 const MAX = 200;
-
+const PHOTOS = [];
+checkStringLength(STR, MAXLENGTH);
 const createPhotos = () => {
-  const Photos = [];
+
   let ArrLength = 25;
   for (let i = 1; i <= ArrLength; i++) {
 
-    Photos.push({
+    PHOTOS.push({
       id: i,
       url: `photos/${i}.jpg`,
       description: `user${i} say ${STR}`,
       likes: getRandomBetween(MIN, MAX),
-      comments: ourComments(i),
+      comments: getOurComments(i),
     })
   }
-  for (let j = 0; j < ArrLength; j++) {
-    return (Photos[j]);
-  }
+  let allPhotos = PHOTOS;
+  return allPhotos;
 }
 
 createPhotos();
