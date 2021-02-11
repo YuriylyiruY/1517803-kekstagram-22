64175a4eb4b7ff4228d9bbf18c8a6b3425f8e97f
YuriylyiruY <ykomendantskiy@list.ru>
2021-02-11 21:24:16 +0400

    доработка кода 5

--- js/main.js
+++ js/main.js
@@ -7,8 +7,13 @@ const STR = 'good';
 const MAXLENGTH = 9;
 const MIN = 15;
 const MAX = 200;
-
 const ARRLENGTH = 25;
+const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
 const getRandomBetween = function (fromNum, toNum) {
 
   if (isNaN(toNum) || isNaN(fromNum) || Array.isArray(toNum) || Array.isArray(fromNum) || fromNum < 0 || toNum < 0) {
@@ -28,11 +33,7 @@ const checkStringLength = function (STR, MAXLENGTH) {
   return (STR.length <= MAXLENGTH);
 }
 
-const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'В целом всё неплохо. Но не всё.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
+
 const getMessageText = () => {
 
   const randArray = getRandomBetween(0, MESSAGE.length - 1);
@@ -40,19 +41,14 @@ const getMessageText = () => {
 };
 
 const getOurComments = (i) => {
-
-
-  const COMMENT = {
+  return {
     id: getRandomBetween(START_NUM_ID, END_NUM_ID),
     avatar: `img/avatar-${getRandomBetween(START_NUM_AVATAR, END_NUM_AVATAR)}.jpg`,
     message: getMessageText(),
     name: `user${i}`,
   }
-  return COMMENT;
-
 }
 
-checkStringLength(STR, MAXLENGTH);
 const createPhotos = () => {
 
   const PHOTOS = [];
@@ -70,4 +66,5 @@ const createPhotos = () => {
   return PHOTOS;
 }
 
+checkStringLength(STR, MAXLENGTH);
 createPhotos();
